61857530e29d97efe8e16e825aec824f9991d24b
Alisson-grem <alisson.agp.rj@gmail.com>
2025-06-27 22:51:23 -0300

    Atualizado

---
+++ components/Tempo.tsx
@@ -0,0 +1,13 @@
+import {View, Text, StyleSheet} from 'react-native';
+
+export default function Tempo(){
+
+    return(
+        <View>
+                <Text>Previsão para o dia: </Text>
+                <Text>Máx: </Text>
+                <Text>Mín: </Text>
+                <Text></Text>
+        </View>
+    )
+}
